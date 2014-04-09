01666e05d02f7fb4e2efc9fa490821260127510d
Maikol <miguel.deelias@gmail.com>
2014-04-09 12:43:39 -0300

    changed some stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,21 +11,10 @@ set :repository, "git@github.com:Maikol/rails_start.git"
 set :user, "ubuntu"
 set :use_sudo, false
 set :ssh_options, { :forward_agent => true }
-set :branch, "chef_repo"
+set :deploy_to, "/home/ubuntu/"
 
 server "ec2-54-207-68-229.sa-east-1.compute.amazonaws.com", :web, :app, :db, :primary => true
 ssh_options[:keys] = ["/Users/migueldeelias/Downloads/de-forms.pem"]
-
-after "deploy:finalize_update", "symlink:all"
-
-namespace :symlink do
-  task :db do
-    run "ln -nfs /u/apps/de-fomularios_production/shared/config/database.yml #{release_path}/config/database.yml"
-  end
-  task :all do
-    symlink.db
-  end
-end
 
 namespace :deploy do
 
